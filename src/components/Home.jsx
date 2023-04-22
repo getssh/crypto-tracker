41dba5a64ba433308d23b40c6607305dda1b0db2
getssh <gtamrat33@gmail.com>
2023-04-22 14:30:42 +0300

    Add style for home page elements with a router link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import {useSelector, useDispatch} from 'react-redux'
 import { useEffect } from 'react';
 import { getHome } from '../redux/homeSlice';
+import { Link } from 'react-router-dom';
 
 
 const Home = () => {
@@ -17,24 +18,29 @@ const Home = () => {
 
   return (
     <div>
-
+      <h3 className='bg-[#e74886] text-white'>STATS BY TRENDING COIN</h3>
       {
         trending &&
-        <div className='grid grid-cols-2'>
-          {
-           trending.map((coin) => {
-            return (
-              <div key={coin.coin_id}>
-              <span>
-                  <img src={coin.img} alt="coin iamge"/>
-              </span>
-              <p>{coin.name}</p>
-              <p>{coin.market_cap_rank}</p>
-              </div>
-            )
-           })
-          }
-        </div>
+        <Link to='/coin'>
+          <div className='grid grid-cols-2 justify-center home-grid'>
+            {
+            trending.map((coin, i) => {
+              const evenClass = `flex flex-col p-3 bg-[#e74886]`;
+              const oddClass = `flex flex-col p-3 bg-[#d04379]`;
+              const gridClass = i % 2 == 0 ? evenClass : oddClass;
+              return (
+                <div key={coin.coin_id} className={gridClass}>
+                <span className='self-center pr-6'>
+                    <img src={coin.img} alt="coin image"/>
+                </span>
+                <h3 className='text-white self-end'>{coin.name}</h3>
+                <p className='text-white self-end'>{coin.market_cap_rank}</p>
+                </div>
+              )
+            })
+            }
+          </div>
+        </Link>
       }
     </div>
   )
