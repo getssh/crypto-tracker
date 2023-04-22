1af6e798d30ea2704e54c6b2c4d0a6bc25d834ab
getssh <gtamrat33@gmail.com>
2023-04-22 11:19:12 +0300

    Configure components with router and render

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import CoinDetail from './components/CoinDetail';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className='App'>
-      <h2>Hello</h2>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coin" element={<CoinDetail />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
