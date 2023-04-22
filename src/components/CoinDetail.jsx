c7fbabcc4da6e9e907ad190ebe8b10ffcb761a56
getssh <gtamrat33@gmail.com>
2023-04-22 15:37:46 +0300

    Add more data from store

--- src/components/CoinDetail.jsx
+++ src/components/CoinDetail.jsx
@@ -11,14 +11,43 @@ const CoinDetail = () => {
   useEffect(() => {
     dispatch(getCoin(params.id));
   }, [dispatch])
+
+  const tempDAta = Object.keys(coins.market_data.current_price).map(key => {
+
+    return (
+    <div key={key}>{params.id} vs {key.toUpperCase()} {coins.market_data.current_price[key]}</div>
+    )
+  }
+  )
+  
+  console.log(params);
+  console.log(coins);
   return (
     <div>
       {
-        coins &&
-        <p>{coins.id}</p>
+      coins &&
+      <div>
+        <>
+          <div>{coins.id}</div>
+          <div>{coins.market_cap_rank}</div>
+          <div>{coins.market_data.high_24h.usd}</div>
+          <div>{coins.market_data.low_24h.usd}</div>
+          <div>{coins.market_data.circulating_supply}</div>
+          <div>{coins.market_data.current_price.usd}</div>
+          <div>{coins.market_data.price_change_percentage_1y.toFixed(2)}%</div>
+          <div>{coins.coingecko_rank}</div>
+          <div>{coins.community_score}</div>
+          <div>{coins.liquidity_score}</div>
+          <div>
+            <div>
+              {tempDAta}
+            </div>
+          </div>
+        </>
+      </div>
       }
     </div>
   )
 }
 
-export default CoinDetail
+export default CoinDetail;
