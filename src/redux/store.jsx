6aca600e70635100dab08aac4ad3c8503b0c2374
getssh <gtamrat33@gmail.com>
2023-04-22 15:37:14 +0300

    Add coinReducer to store

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import trendingReducer from './homeSlice';
+import coinsReducer from './coinSlice';
 
 const store = configureStore({
   reducer: {
     trending: trendingReducer,
+    coins: coinsReducer,
   }
 });
 
