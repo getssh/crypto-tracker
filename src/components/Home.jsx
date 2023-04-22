7419fa68dafef3a49a84ac48d0a191be59c4d7c5
getssh <gtamrat33@gmail.com>
2023-04-22 15:38:14 +0300

    Update router Link with params

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
       <h3 className='bg-[#df4783] text-white'>STATS BY TRENDING COIN</h3>
       {
         trending &&
-        <Link to='/coin'>
+        <>
           <div className='grid grid-cols-2 justify-center home-grid'>
             {
             trending.map((coin, i) => {
@@ -29,22 +29,24 @@ const Home = () => {
               const oddClass = `flex flex-col p-3 bg-[#d04379]`;
               const gridClass = i % 2 == 0 ? evenClass : oddClass;
               return (
-                <div key={coin.coin_id} className={gridClass}>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 self-end text-white">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                  </svg>
+                <Link to={`${coin.id}`} key={coin.coin_id}>
+                  <div className={gridClass}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 self-end text-white">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
 
-                  <span className='self-center pr-6'>
-                      <img src={coin.img} alt="coin image"/>
-                  </span>
-                  <h3 className='text-white self-end'>{coin.name}</h3>
-                  <p className='text-white self-end'>{coin.market_cap_rank}</p>
-                </div>
+                    <span className='self-center pr-6'>
+                        <img src={coin.img} alt="coin image"/>
+                    </span>
+                    <h3 className='text-white self-end'>{coin.name}</h3>
+                    <p className='text-white self-end'>{coin.market_cap_rank}</p>
+                  </div>
+                </Link>
               )
             })
             }
           </div>
-        </Link>
+        </>
       }
     </div>
   )
